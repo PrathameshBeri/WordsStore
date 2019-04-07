6dbd7121bf80cd7f91cd5ba0c4c823b2495e507e
unknown <prathamesh_beri@protonmail.com>
2019-04-07 23:59:17 +0530

    changes to the DTO

--- src/main/java/com/words_store/home_dto/homeControllerDTO.java
+++ src/main/java/com/words_store/home_dto/homeControllerDTO.java
@@ -1,11 +1,32 @@
 package com.words_store.home_dto;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.fasterxml.jackson.annotation.JsonSetter;
 
 
 public class homeControllerDTO {
 
 	
+	@JsonSetter("name")
 	
+	private String name;
+	
+	@JsonSetter("Descritption")
+	private String description;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	
 }
