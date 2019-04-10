01e3c9c7ce88379f3441c5fb174df9336db07138
unknown <prathamesh_beri@protonmail.com>
2019-04-10 23:32:40 +0530

    fixing project setup

--- src/main/java/com/words_store/home_controller/homeController.java
+++ src/main/java/com/words_store/home_controller/homeController.java
@@ -1,8 +1,31 @@
 package com.words_store.home_controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.words_store.home_controller_service.homeControllerServiceImpl;
+import com.words_store.home_repository.homeControllerRepo;
+
+
+
+@RestController
 @RequestMapping(value = "/")
 public class homeController {
+	
+	@Autowired
+	homeControllerServiceImpl homeControllerService;
 
+	
+	@GetMapping
+	public ResponseEntity getAllWords() {
+		
+		List<homeControllerRepo> words = homeControllerService.findAll();
+		return new ResponseEntity(words, HttpStatus.CREATED);
+	}
 }
